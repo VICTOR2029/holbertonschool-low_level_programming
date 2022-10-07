28f3999d4010997c15d39e8fd5923b63d8fd7eb6
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-07 14:29:22 -0700

    borrado

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,19 +1,21 @@
 /*
  * File: 5-printf.c
- * Auth: Brennan D Baraban
+ * Auth: Brennan D Beraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is  piece of art," followed by a new line.
- *               
+ *main - Prints "wint proper gramma, but the octcome is a piece of art,"followed by a new line.
  *
- * Return: Always 0.
+ *
+ *Return: Always 0.
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+  printf("with proper grammar, but the outtcome is a piece of art,\n")
+    return
 
-  return (0);
-}
+    }
+
+    
