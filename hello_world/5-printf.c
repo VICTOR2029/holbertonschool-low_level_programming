69d3e0dd50a6788524b8b2a19ba3b9bf62abc3eb
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-09 18:15:34 -0700

    script

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,23 +1,15 @@
-/*
- * File: 5-print_numbers.c
- * Auth: Brennan D Baraban
- */
-
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /* betty style doc for function main goes there */
 /**
- * 
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int num;
-
-  for (num = 0; num < 10; num++)
-    printf("%d", num);
-
-  printf("\n");
-
-  return (0)
+  printf("0123456789\n");
+  return (0);
+}
