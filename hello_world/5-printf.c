63692cc817f48af281c033feab7ec8df5089ae1b
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-09 17:28:50 -0700

    script

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,13 +3,7 @@
  * Auth: Brennan D Baraban
  */
 
-#include <stdio.h>
-
-/**
- * main - Prints all single digit numbers of base 10 starting from 0.
- *
- * Return: Always 0.
- */
+#include <stdio.h 
 
 int main(void)
   
