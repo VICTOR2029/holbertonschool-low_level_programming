19bb241056ab23a47e6e5e3d03e38297a331c9e2
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-09 18:36:35 -0700

    script

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,25 +1,24 @@
-/*
- * File: 6-size.c
- * Auth: Brennan D Baraban
- */
-
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
- * main - Prints the size of various types based on
- *        the computer it is compiled and run on..
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  printf("Size of a char: %zu byte(s)\n", sizeof(char));
-  printf("Size of an int: %zu byte(s)\n", sizeof(int));
-  printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-  printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-  printf("Size of a float: %zu byte(s)\n", sizeof(float));
+  int i;
+
+  i = 48;
+  while  (i < 58)
+    {
+      putchar(i);
+      i++;
+    }
+  putchar('\n');
 
   return (0);
 }
-
-
