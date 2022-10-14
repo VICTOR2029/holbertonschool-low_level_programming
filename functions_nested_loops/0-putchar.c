603284f9236f7fe7911c522b7572af739597f96e
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-13 17:56:51 -0700

    C - Functions

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  print_putchar();
+  char a[] = "Holberton";
+  int b;
+
+  for (b = 0; b < 9; b++)
+    _putchar(a[b]);
+  _putchar('\n');
   return (0);
 }
 
