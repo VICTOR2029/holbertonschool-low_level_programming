af9002c824d461e7747762d67d37eaf3be90b5e0
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-19 08:51:55 -0700

    main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+What is _putchar?
+putchar is a function in the C programming language that writes a single character to the standard output stream, stdout . Its prototype is as follows: int putchar (int character) The character to be printed is entered into the function as an argument and, if the write is successful, the character in the argument is returned.
+  
