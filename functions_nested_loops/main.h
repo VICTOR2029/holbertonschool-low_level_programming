d7048216e151afcb176d4df0cefd79c5f8538363
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-20 16:40:22 -0700

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,14 +1,19 @@
-# include  < unistd.h >
-
-/* *
- * _putchar - escribe el carácter c en stdout
- * @c: El caracter a imprimir
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * file hholberton.h
  *
- * Regreso: En caso de éxito 1.
- * En caso de error, se devuelve -1 y errno se establece correctamente.
+ * Desc: Header fil containing declarations
  */
-int  _putchar ( char c)
-{
-  volver ( escribir ( 1 , &c, 1 ));
-}
-  
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#endif
