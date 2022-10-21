28e57a3875f431a2548967251026722017a6ccab
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-20 17:21:44 -0700

    C - Functions

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Description: Prints _putchar with _putchar
- *
- * Return: always 0
- */
+* main - entry point
+*
+* Description: Prints _putchar with _putchar
+*
+* Return: always 0
+*/
 
 int main(void)
 {
-  int i = 0;
-  char c;
-  char s[] = "putchar \n";
+ int i = 0;
+char c;
+char s[] = "putchar \n";
 
-  while (i <= 9)
-    {
-      c = s[i];
-      _putchar(c);
-      i++;
-    }
-  return (0);
+while (i <= 9)
+{
+c = s[i];
+_putchar(c);
+i++;
+}
+return (0);
 }
