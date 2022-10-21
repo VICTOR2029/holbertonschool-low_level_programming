313997f62ee01464715473762822a9f32a59d2ae
VICTOR2029 <sierrayepezv@gmail.com>
2022-10-20 17:07:03 -0700

    C - Functions

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,15 @@
 #include "main.h"
-
 /**
-* print_alphabet - entry point
-*
-* Description: Prints the alphabet with _putchar
-*
-* Return: void
-*/
-
-
-void print_alphabet(void)
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+void print_alphabet(void);
 {
-char c;
+  char c;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+  for (c = 'a'; c <= 'z'; c++)
+    {
+      _putchar(c);
+    }
+  _putchar('\n');
 }
