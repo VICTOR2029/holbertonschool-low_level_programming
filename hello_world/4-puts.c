e6f3f560fdeb3ae5a0c6e8a7cfac933e96135684
VICTOR2029 <sierrayepezv@gmail.com>
2023-10-31 10:36:16 -0700

    4-puts.c 07

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main punto de entrada
- *
+ 
  * descricion: usando la funcion pust
- *
+ 
  * parametro: se usa la funcion main
- *
+ 
  * Return: Always 0 (Success)
+ 
  */
 
 int main(void)
