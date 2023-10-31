fc1b98f1576dab502996001a501d8ee4cf7f9da7
VICTOR2029 <sierrayepezv@gmail.com>
2023-10-30 18:23:11 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stido.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+pust("\"programming is like building a multilingual puzzle");
+return (0);
+}
